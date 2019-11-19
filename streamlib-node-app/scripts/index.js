cda4d6530fe729d7a9558317f333d9f603b5d5c9
Markus Feng <markusfeng@gmail.com>
2019-11-18 23:32:00 -0500

    Added electron and simple Twitch auth

--- streamlib-node-app/scripts/index.js
+++ streamlib-node-app/scripts/index.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+
+function createWindow() {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    let authHost = 'localhost';
+    let authPath = '/';
+
+    win.webContents.on('will-redirect', function(event, urlString) {
+        console.log('Redirect');
+        const urlObject = url.parse(urlString);
+        console.dir(urlObject);
+        if (urlObject.hostname === authHost && urlObject.pathname === authPath) {
+            console.log('Auth redirect');
+            event.preventDefault();
+            win.loadFile('page.html');
+        }
+    });
+    win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
