58619c15eccc0d46e62c672b43581f657c3987d1
Markus Feng <markusfeng@gmail.com>
2019-11-19 01:33:16 -0500

    Added channel info

--- streamlib-node-app/scripts/index.js
+++ streamlib-node-app/scripts/index.js
@@ -1,6 +1,8 @@
 var electron = require('electron');
 var authTokenStorage = 'StreamLib/auth-token';
 
+var authToken;
+
 function main() {
     var riotAPI = new RuneterraAPI();
     console.log('Running');
@@ -11,13 +13,23 @@ function main() {
         console.log('Access token: ' + accessToken);
         window.localStorage.setItem(authTokenStorage, accessToken);
     }
-    var authToken = window.localStorage.getItem(authTokenStorage);
+    authToken = window.localStorage.getItem(authTokenStorage);
     if (authToken) {
         document.getElementById('auth').innerHTML = 'Authenticated.';
     }
     else {
         document.getElementById('auth').innerHTML = 'Not authenticated.';
     }
+}
+
+function getAuthToken() {
+    return authToken;
+}
+
+function printInfo() {
+    getInfo(getAuthToken(), function(info) {
+        console.log(info);
+    });
 }
 
 main();
--- streamlib-node-app/scripts/twitch.js
+++ streamlib-node-app/scripts/twitch.js
@@ -4,19 +4,27 @@ function makeHeaders(token) {
     return {
         'Accept': 'application/vnd.twitchtv.v5+json',
         'Client-ID': 'yh4epzuv85ntcpyl6s38ompr7h5msa',
-        'Authorization': token
+        'Authorization': 'OAuth ' + token
     };
 }
 
-function getInfo(token) {
-    var channelID;
+function getChannelInfo(token, callback) {
     var options = {
         url: 'https://api.twitch.tv/kraken/channel',
         headers: makeHeaders(token)
     }
     request.get(options, function (error, response, body) {
-        var bodyObject = JSON.parse(body);
-        channelID = bodyObject["_id"];
+        console.log('Get channel status code ' + response.statusCode);
+        var b = JSON.parse(body);
+        var resp = {
+            twitchID: b["_id"],
+            url: b["url"],
+            name: b["display_name"],
+            language: b["broadcaster_language"],
+            mature: b["mature"],
+            logo: b["logo"],
+            streams: 
+        };
+        callback(resp);
     });
-    return channelID;
 }
